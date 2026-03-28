1ba67a68b25805c62ed6ae60895230149536d405
Daniil Yankouski <yankouski.daniil@gmail.com>
2026-03-28 18:38:30 +0100

    refactor(validation): Update tokenizer type hints to use TokenizerLike

--- vllm/validation.py
+++ vllm/validation.py
@@ -1,11 +1,8 @@
+from typing import Any, Dict, List, Optional
+
 from pydantic import BaseModel
-from vllm.transformers_utils.tokenizer import (
-    AnyTokenizer,
-)
-
-
-from typing import Any, Dict, Optional, List
 from pydantic import Field
+from vllm.tokenizers import TokenizerLike
 
 
 class EnforcedToken(BaseModel):
@@ -15,7 +12,7 @@ class EnforcedToken(BaseModel):
     token_id: Optional[int] = Field(default=None, exclude=True)
     top_token_ids: List[int] = Field(default_factory=list, exclude=True)
 
-    def encode(self, tokenizer: AnyTokenizer) -> List[int]:
+    def encode(self, tokenizer: TokenizerLike) -> List[int]:
         try:
             self.token_id = int(self.token)
             self.top_token_ids = [int(t) for t in self.top_tokens]
@@ -26,7 +23,7 @@ class EnforcedToken(BaseModel):
 class EnforcedTokens(BaseModel):
     tokens: List[EnforcedToken]
 
-    def encode(self, tokenizer: AnyTokenizer):
+    def encode(self, tokenizer: TokenizerLike):
         for token in self.tokens:
             token.encode(tokenizer)
 
